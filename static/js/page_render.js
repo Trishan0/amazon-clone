51cb1e47d36a206a7a05b45aaa31b7b27bd2a67d
Trishan <sanjanatrishan@gmail.com>
2025-04-07 10:41:44 +0530

    Change external js to internal js

--- static/js/page_render.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("navbar-placeholder").innerHTML = fetch('pages/navbar.html')
-    .then(res => res.text())
-    .then(data => document.getElementById("navbar-placeholder").innerHTML = data);
-
-document.getElementById("footer-placeholder").innerHTML = fetch('pages/footer.html')
-    .then(res => res.text())
-    .then(data => document.getElementById("footer-placeholder").innerHTML = data);
